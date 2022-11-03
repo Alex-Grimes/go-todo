b2708a1a5f93ea8079039b96171d54f3ad4cfb6a
Alex <AlexGrimes.se@gmail.com>
2022-11-02 21:17:28 -0400

    setup db connection

--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module go-todo
 go 1.19
 
 require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/sirupsen/logrus v1.9.0 // indirect
 	golang.org/x/sys v0.1.0 // indirect
 )
--- todolist.go
+++ todolist.go
@@ -4,9 +4,18 @@ import (
 	"io"
 	"net/http"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 	log "github.com/sirupsen/logrus"
 )
+
+type TodoItemModel struct {
+	Id          int `gorm:"primary_key"`
+	Description string
+	Completed   bool
+}
 
 func Health(w http.ResponseWriter, r *http.Request) {
 	log.Info("API Health is OK")
@@ -20,6 +29,12 @@ func init() {
 }
 
 func main() {
+	db, _ := gorm.Open("mysql", "root:root@/todolist?charset=utf8&parseTime=True&loc=Local")
+
+	defer db.Close()
+	db.Debug().DropTableIfExists(&TodoItemModel{})
+	db.Debug().AutoMigrate(&TodoItemModel{})
+
 	log.Info("Starting Todolist API server")
 	router := mux.NewRouter()
 	router.HandleFunc("/health", Health).Methods("GET")
