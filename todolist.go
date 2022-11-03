df6f311ab57abea3d952a227d6600313a4459e45
Alex <AlexGrimes.se@gmail.com>
2022-11-02 20:34:58 -0400

    server health function to confirm its up

--- todolist.go
+++ todolist.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"io"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
+)
+
+func Health(w http.ResponseWriter, r *http.Request) {
+	log.Info("API Health is OK")
+	w.Header().Set("Content-Type", "application/json")
+	io.WriteString(w, `{"alive": true}`)
+}
+
+func init() {
+	log.SetFormatter(&log.TextFormatter{})
+	log.SetReportCaller(true)
+}
+
+func main() {
+	log.Info("Starting Todolist API server")
+	router := mux.NewRouter()
+	router.HandleFunc("/health", Health).Methods("GET")
+	http.ListenAndServe(":8000", router)
+}
