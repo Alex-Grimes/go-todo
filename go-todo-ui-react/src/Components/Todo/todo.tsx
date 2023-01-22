718f9bb6fce96b76752f9529941cd053989cfa5d
Alex <AlexGrimes.se@gmail.com>
2023-01-22 17:44:32 -0500

    correcting css identifying missing pieces for port

--- go-todo-ui-react/src/Components/Todo/todo.tsx
+++ go-todo-ui-react/src/Components/Todo/todo.tsx
@@ -3,33 +3,32 @@ import "../Todo/todo.css"
 
 export const Todo = () => {
     return (
-        <div>
-        <h1>Todos </h1>
+<>
+<main>
+<h1>Todos</h1>
 
-        < button className="clear" >
-            Clear completed todos
-                </button>
-
-                < div className="prog" >
-                        < p >
-                                <b></b>< b ></b> completed
-                        </p>
-                </div>
-        < ul className="todos">
-            <li className="todo" >
-                <input type="checkbox"/>
-                <label></label>
-                </li>
-                </ul>
-
-            < form className="addForm" >
-                <label> Add todo </label>
-                    < div className= "sl" >
-                        <input type= "text" name = "add" id = "add" />
-                            <button type="submit" > Add </button>
-                    </div>
-            </form>
-        </div>
+<button className="clear">
+Clear completed todos
+</button>
+<div className="prog" >
+<p>
+<b></b> out of
+<b></b> completed
+</p>
+</div>
+    <ul className="todos">
+<li className="todo">
+<input type="checkbox" name="isCompleted" />
+<label></label>
+</li>
+</ul><form className="addForm">
+<label>Add todo</label><div className="sl">
+<input type="text" name="add" id="add" v-model="currentTodoInp" />
+<button type="submit">Add</button>
+</div>
+</form>
+</main>
+</>
   )
 }
 
